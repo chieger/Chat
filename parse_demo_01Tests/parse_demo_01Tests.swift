02f9928c0247e4a74f09dc06aac26bb84247597c
chieger <charlie@codepath.com>
2017-06-25 17:20:23 -0700

    Initial Commit

---
+++ parse_demo_01Tests/parse_demo_01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  parse_demo_01Tests.swift
+//  parse_demo_01Tests
+//
+//  Created by Charles Hieger on 6/25/17.
+//  Copyright © 2017 Charles Hieger. All rights reserved.
+//
+
+import XCTest
+@testable import parse_demo_01
+
+class parse_demo_01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
